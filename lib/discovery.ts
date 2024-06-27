5818fd3da9bb462f7ba2aacccf443407f7b85b6d
Rishabh Singh <pingrishabh@gmail.com>
2024-06-27 16:59:09 +0530

    ✨ added clickhouse

--- lib/discovery.ts
+++ lib/discovery.ts
@@ -1,5 +1,8 @@
+// The key should match the component name in the library
+
 export const ComponentExpressionList = {
   Nginx: "nginx_up",
   PostgreSQL: "pg_up == 0",
   Kubernetes: "kube_node_status_condition",
+  Clickhouse: "clickhouse_up",
 };
