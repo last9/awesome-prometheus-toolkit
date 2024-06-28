adf23cfdbb547368cd5c8c8c1323ff71e58aed51
Rishabh Singh <pingrishabh@gmail.com>
2024-06-28 11:09:16 +0530

    ✨ added elasticsearch

--- lib/discovery.ts
+++ lib/discovery.ts
@@ -5,4 +5,5 @@ export const ComponentExpressionList = {
   PostgreSQL: "pg_up == 0",
   Kubernetes: "kube_node_status_condition",
   Clickhouse: "clickhouse_up",
+  Elasticsearch: "elasticsearch_node_stats_up",
 };
